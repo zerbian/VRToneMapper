9ad5ea6122f35b443194777a139c4591a5a79633
zerbian <38952981+zerbian@users.noreply.github.com>
2022-03-28 16:27:51 +0200

    split gaze position in x y for better handling
    maybe revert this, because of semantics

--- VRToneMapper.cs
+++ VRToneMapper.cs
@@ -16,7 +16,7 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
     {
         None,
 
-        [InspectorName("Reinhard without Gaze")]
+        [InspectorName("Standard Reinhard")]
         Reinhard,
 
         [InspectorName("Reinhard with Gaze")]
@@ -31,15 +31,21 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
     [Serializable]
     public sealed class ToneMappingOperatorTypeParamter : VolumeParameter<ToneMappingOperatorType>
     {
-        public ToneMappingOperatorTypeParamter(ToneMappingOperatorType value, bool overrideState = false) : base(value, overrideState) {}
+        public ToneMappingOperatorTypeParamter(ToneMappingOperatorType value, bool overrideState = false) : base(value, overrideState) { }
     }
-    
+
     [Header("Gaze Details")]
     /// <summary>
     /// The gaze position in screenspace. This should be the target for the Eyetracker
     /// </summary>
-    [Tooltip("Center point of the gaze point")]
-    public Vector2Parameter m_GazeCenter = new Vector2Parameter(new Vector2(0.5f, 0.5f));
+    [Tooltip("Center point of the gaze point: X")]
+    public ClampedFloatParameter m_GazeCenterX = new ClampedFloatParameter(0.1f, 0.0f, 1.0f);
+    /// <summary>
+    /// The gaze position in screenspace. This should be the target for the Eyetracker
+    /// </summary>
+    [Tooltip("Center point of the gaze point: Y")]
+    public ClampedFloatParameter m_GazeCenterY = new ClampedFloatParameter(0.1f, 0.0f, 1.0f);
+
 
     /// <summary>
     /// The area surrounding the gaze point gets weighted with a Gaussian function. The sigma value controls the radius. Number is in pixel space.
@@ -65,11 +71,10 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
     /// 
     /// </summary>
     [Tooltip("")]
-    public FloatParameter m_LuminanceScale = new FloatParameter(1.0f);
+    public FloatParameter m_LuminanceKey = new FloatParameter(1.0f);
 
-    public FloatParameter m_TargetLuminance = new FloatParameter(1.0f);
 
-    
+
 
     /// <summary>
     /// Shows the Gaussian Weight function
@@ -77,6 +82,9 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
     [Header("Debugging")]
     [Tooltip("Debug Gaussian Weighting Function")]
     public BoolParameter m_ShowMask = new BoolParameter(false);
+
+    [Tooltip("Parameter for Debugging")]
+    public FloatParameter m_DebugParamter = new FloatParameter(0.5f);
 
     /// <summary>
     /// Reference to the Matrial aka. the ShaderLab / HLSL Shader file
@@ -100,7 +108,8 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
     /// Utility for using shader pass names inside CommandBuffer.Blit
     /// </summary>
     /// <param name="passName">Name of pass inside the shader</param>
-    private int PASS(String passName) {
+    private int PASS(String passName)
+    {
         int pass = m_Material.FindPass(passName);
         if (pass == -1) throw new Exception(passName + " does not exist in the material/shader");
         return pass;
@@ -136,17 +145,18 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
             return;
 
         //m_Material.SetFloat("_Intensity", intensity.value);
-        m_Material.SetVector("_GazeCenter", m_GazeCenter.value);
+        m_Material.SetVector("_GazeCenter", new Vector2(m_GazeCenterX.value,m_GazeCenterY.value));
         m_Material.SetFloat("_Sigma", m_Sigma.value);
         m_Material.SetFloat("_AdaptationScale", m_AdaptationScale.value);
-        m_Material.SetFloat("_LuminanceKey", m_LuminanceScale.value);
-        m_Material.SetFloat("_TargetLuminance", m_TargetLuminance.value);
+        m_Material.SetFloat("_LuminanceKey", m_LuminanceKey.value);
+        m_Material.SetFloat("_DebugParamter", m_DebugParamter.value);
 
 
         m_Material.SetTexture("_SourceTexture", source);
 
         //------------SHOW-MASK-PASS---------------------------------------------------------------
-        if (m_ShowMask.value) {
+        if (m_ShowMask.value)
+        {
             cmd.Blit(source, destination, m_Material, PASS("Mask"));
             return;
         }
@@ -168,7 +178,8 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
         m_Material.SetInteger("_MaxMipLevel", maxMip);
         m_Material.SetTexture("_MippedTexture", t_MippedSource);
 
-        if (m_FirstFrame) {
+        if (m_FirstFrame)
+        {
             Debug.Log("First Frame");
             m_FirstFrame = false;
             cmd.Blit(null, m_LuminanceTextureHandle, m_Material, PASS("Init"));
@@ -191,10 +202,11 @@ public sealed class VRToneMapper : CustomPostProcessVolumeComponent, IPostProces
         RenderTexture.ReleaseTemporary(t_MippedSource);
     }
 
-    public override void Cleanup() {
+    public override void Cleanup()
+    {
         RTHandles.Release(m_LuminanceTextureHandle);
 
         CoreUtils.Destroy(m_Material);
-    } 
+    }
 
 }
